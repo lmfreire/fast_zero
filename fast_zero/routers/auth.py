67870bb0ab83062431cdecea75c2148332ad4dce
lmFreire <l.matheus.freire10@gmail.com>
2025-04-03 23:05:06 -0300

    alguns teste e renovando login

--- fast_zero/routers/auth.py
+++ fast_zero/routers/auth.py
@@ -9,14 +9,12 @@ from sqlalchemy.orm import Session
 from fast_zero.database import get_session
 from fast_zero.models import User
 from fast_zero.schemas import Token
-from fast_zero.security import (
-    create_access_token,
-    verify_password,
-)
+from fast_zero.security import create_access_token, get_current_user, verify_password
 
 router = APIRouter(prefix='/auth', tags=['auth'])
 OAuth2Form = Annotated[OAuth2PasswordRequestForm, Depends()]
 T_Session = Annotated[Session, Depends(get_session)]
+T_CurrentUser = Annotated[User, Depends(get_current_user)]
 
 
 @router.post('/token/', response_model=Token)
@@ -39,3 +37,10 @@ def login_for_access_token(
     access_token = create_access_token(data={'sub': user.email})
 
     return {'access_token': access_token, 'token_type': 'bearer'}
+
+
+@router.post('/refresh_token', response_model=Token)
+async def refresh_access_token(user: T_CurrentUser):
+    new_access_token = create_access_token(data={'sub': user.email})
+
+    return {'access_token': new_access_token, 'token_type': 'bearer'}
